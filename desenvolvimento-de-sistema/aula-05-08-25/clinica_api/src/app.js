6ce538b101064e6102a2a64a649311daab9dfad2
CamilaListon <c.liston@hotmail.com>
2025-08-06 21:50:27 -0300

    aula desenvolvimento de sistemas 06/08/2025

--- desenvolvimento-de-sistema/aula-05-08-25/clinica_api/src/app.js
+++ desenvolvimento-de-sistema/aula-05-08-25/clinica_api/src/app.js
@@ -8,4 +8,15 @@ app.use('/',(req, res)=>{
     return res.json("Olá")
 })
 
+app.get('usuarios', async (req, res) => {
+    try{
+        const usuarios = await Prisma.post.fundMany();
+        return res.json(usuarios)
+    }
+    catch (e){
+        console.log(e)
+    }
+}
+)
+
 app.listen(3000, ()=> console.log("Api Rodando"))
