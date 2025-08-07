c1c9d628b640450578a687662ff9e22af77c59f4
CamilaListon <c.liston@hotmail.com>
2025-08-06 21:28:08 -0300

    aula desenvolvimento de sistemas 06/08/2025

--- desenvolvimento-de-sistema/aula-05-08-25/clinica_api/prisma/seed.js
+++ desenvolvimento-de-sistema/aula-05-08-25/clinica_api/prisma/seed.js
@@ -4,38 +4,45 @@ const prisma = new PrismaClient();
 async function main() {
     await prisma.usuario.createMany({
         data:[
-            {email: "joao@email.com",  password: "123"},
+            {   nome: "Joao",
+                email: "455@email.com",  
+                senha: "123",
+                cargo: "Médico"
+            },
         ],
     });
 
     await prisma.paciente.create({
-        nome:"João",
-        sexo: "Masculino",
-        data_nascimento: new Date ("1980-12-11"),
-        cpf: "958.521.251.65",
-        telefone: "(48)9525-56232",
-        email: "joao@email.com",
-        consulta:{
-            create:[
-                {
-                    motivo: "Dor nas costas",
-                    data_consulta: new Date ("2023-08-25"),
-                    observacoes: "Ibuprofeno 3x ao dia",
-                }
-            ]
-        },
-        exame:{
-            create: [
-                {
-                    tipo: "Densiometro",
-                    data_exame: new Date ("2023-08-25"),
-                    type: "clinico",
-                    resultado: "deu ruim",
-                    link_arquivo: "url.com.br/exame.pdf",
-                    observacoes: ""
-                }
-            ]
-        },
+        data: {
+
+            nome:"Joao",
+            sexo: "Masculino",
+            data_nascimento: new Date ("1980-12-11"),
+            cpf: "958.521.251.65",
+            telefone: 99999,
+            email: "joao132@email.com",
+            // consulta:{
+            //     create:[
+            //         {
+            //             motivo: "Dor nas costas",
+            //             data_consulta: new Date ("2023-08-25"),
+            //             observacoes: "Ibuprofeno 3x ao dia",
+            //         }
+            //     ]
+            // },
+            // exame:{
+            //     create: [
+            //         {
+            //             tipo: "Densiometro",
+            //             data_exame: new Date ("2023-08-25"),
+            //             type: "clinico",
+            //             resultado: "deu ruim",
+            //             link_arquivo: "url.com.br/exame.pdf",
+            //             observacoes: ""
+            //         }
+            //     ]
+            // },
+        }
     });
 }
 
