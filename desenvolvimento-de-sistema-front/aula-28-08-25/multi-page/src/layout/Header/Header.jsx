c415bdb8e4fb305ccf589bfe7e70752e8fc5701f
CamilaListon <c.liston@hotmail.com>
2025-08-28 22:01:36 -0300

    aula front end desenvolvimento de sistemas, att aula perdida do dia 21/08, e novas atividades do dia 28/08

---
+++ desenvolvimento-de-sistema-front/aula-28-08-25/multi-page/src/layout/Header/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { NavLink } from "react-router";
+
+const Header = () => {
+    return (
+        <div>
+            <nav className='flex gap-7'>
+                <NavLink to="/" end>
+                    Home
+                </NavLink>
+                <NavLink to="/sobre" end>
+                    Sobre
+                </NavLink>
+                <NavLink to="/blog" end>
+                    Blog
+                </NavLink>
+                
+            </nav>
+        </div>
+    )
+}
+
+export default Header
