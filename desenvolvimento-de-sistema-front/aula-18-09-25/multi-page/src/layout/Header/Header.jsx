f74457ead51f8674871982bd3204524def7fe536
CamilaListon <c.liston@hotmail.com>
2025-09-18 22:04:41 -0300

    desenvolvimento de sistemas front, aula 18/09/2025

---
+++ desenvolvimento-de-sistema-front/aula-18-09-25/multi-page/src/layout/Header/Header.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { NavLink, useNavigate } from "react-router";
+import { useAuth } from '../../context/AuthContext';
+
+const Header = () => {
+    const { user, logout } = useAuth()
+    const navigate = useNavigate()
+
+    const handleLogout = () => {
+        logout();
+        navigate("/")
+    }
+
+    return (
+        <header className='flex items-center justify-between p-4 bg-gray-200'>
+            <nav className='flex gap-4'>
+
+                <NavLink to="/" end className={({ isActive }) => isActive ? "text-blue-600 font-bold" : "text-gray-800"}>
+                    Home
+                </NavLink>
+
+                <NavLink to="/sobre" end className={({ isActive }) => isActive ? "text-blue-600 font-bold" : "text-gray-800"}>
+                    Sobre
+                </NavLink>
+
+                <NavLink to="/blog" end className={({ isActive }) => isActive ? "text-blue-600 font-bold" : "text-gray-800"}>
+                    Blog
+                </NavLink>
+            </nav>
+
+            <div>
+                {
+                    user ? (
+                        <>
+                            <span className='mr-4'>{user.email}</span>
+                            <button onClick={handleLogout} className='br-red-500 text-white px-3 py-1 rounded'>
+                                Logout
+                            </button>
+                        </>
+                    ) : (
+                        <NavLink to="login" className="bg-blue-600 text-white px-3 py-1 rounded">
+                        </NavLink>
+                    )
+                }
+            </div>
+        </header>
+    )
+}
+
+export default Header
--- desenvolvimento-de-sistema-front/aula-18-09-25/multi-page/src/main.jsx
+++ desenvolvimento-de-sistema-front/aula-18-09-25/multi-page/src/main.jsx
@@ -11,6 +11,8 @@ import Sobre from './pages/Sobre/Sobre';
 import Main from './layout/Main/Main';
 import Blog from './pages/Blog/Blog';
 import PostDetail from './pages/Blog/PostDetail';
+import { AuthProvider } from './context/AuthContext';
+import Login from './pages/Login/Login';
 
 
 const router = createBrowserRouter([
@@ -21,9 +23,14 @@ const router = createBrowserRouter([
       { path: "sobre", element: <Sobre /> },
       { path: "blog", element: <Blog /> },
       { path: "post/:id", element: <PostDetail /> }
-
     ]
+  },
+  {
+    path: "login",
+    element: <Login />
   }
+
+
   // {
   //   path: "/",
   //   element: <Home/>,
@@ -36,7 +43,11 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+
+
     {/* <App /> */}
   </StrictMode>,
 )
