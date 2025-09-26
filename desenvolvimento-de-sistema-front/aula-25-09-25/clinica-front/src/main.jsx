5a96a22d89c346d8f5f32fc2dad4dd5bf3e4c521
CamilaListon <c.liston@hotmail.com>
2025-09-25 22:11:01 -0300

    add aula front, inicio sistema da clinica front end, aula 25/09/25

---
+++ desenvolvimento-de-sistema-front/aula-25-09-25/clinica-front/src/main.jsx
@@ -0,0 +1,28 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+// import reacr router
+import { createBrowserRouter } from 'react-router'
+import { RouterProvider } from 'react-router/dom'
+
+import './index.css'
+import Login from './pages/Login/Login'
+
+
+// import App from './App.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+
+
+    {/* <App /> */}
+  </StrictMode>,
+)
