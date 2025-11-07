8bdac8eda680dea9677d0954df4734fc48700547
CamilaListon <c.liston@hotmail.com>
2025-11-06 21:59:16 -0300

    att

--- desenvolvimento-de-sistema-front/aula-30-10-25/clinica-front/src/components/SideMenu/SideMenu.jsx
+++ desenvolvimento-de-sistema-front/aula-30-10-25/clinica-front/src/components/SideMenu/SideMenu.jsx
@@ -23,7 +23,7 @@ const SideMenu = () => {
 
 
     return (
-        <aside className={`h-screen bg-cyan-800 text-white flex flex-col justify-between transition-all duration-300
+        <aside className={`bg-cyan-800 text-white flex flex-col justify-between transition-all duration-300
         ${isCollapsed ? "w-20" : "w-64"}`}>
 
 
